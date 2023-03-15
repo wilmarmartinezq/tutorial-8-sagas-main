cce038cf5d628307471f4e3b18bd3e3af5d1ec82
wamartinezq <w.martinezq@uniandes.edu.co>
2023-03-15 09:20:45 -0500

    test

--- init.sql
+++ init.sql
@@ -1,10 +1,10 @@
-CREATE TABLE usuarios_legado(  
+CREATE TABLE ordenes(  
     id int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Llave primaria',
-    nombres VARCHAR(255),
-    apellidos VARCHAR(255),
-    direccion VARCHAR(255),
-    telefono VARCHAR(255),
+    nombres_cliente VARCHAR(255),
+    producto VARCHAR(255),
+    cantidad int,
     email VARCHAR(255),
-    password VARCHAR(255)
+    direccion VARCHAR(255),
+    fecha_creacion VARCHAR(255)
 
-) COMMENT 'Tabla legado de los usuarios';
+) COMMENT 'Tabla ordenes';
